82074a61491e443c102b89defe49caac6c9e2b96
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-12-11 21:19:42 +0200

    up

---
+++ storage/framework/views/c73788ebc8e49efcd5622a2459261c2acd4d319c.php
@@ -0,0 +1,8 @@
+
+<footer class="dash-footer">
+  <div class="footer-wrapper">
+    <div class="py-1">
+      <span class="text-muted"> <?php echo e(env('FOOTER_TEXT')); ?></span >
+    </div>
+  </div>
+</footer><?php /**PATH A:\laragon\www\HyperSystem\Projec_Management\codecanyon-24490506-taskly-saas-project-management-tool\main_file\resources\views/partials/footer.blade.php ENDPATH**/ ?>
